e784bccd6c661a4cefe0dc12046d7896370b1f05
Fake <fake@ponyft.com>
2025-05-23 15:43:40 +0800

    简化配置

--- service-consumer-aws/src/main/java/com/example/serviceconsumeraws/config/LoadBalancerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.serviceconsumeraws.config;
-
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@LoadBalancerClients(defaultConfiguration = CustomLoadBalancerConfiguration.class)
-public class LoadBalancerConfig {
-} 
